df1e7b9af313e32e1d21cbf036016a5a8b3bbfe0
MattiaInnocenti <mattia.innocenti1998@gmail.com>
2019-10-31 16:18:35 +0100

    Primo upgrade server, fino alla iterazione item nella cartella passata

---
+++ src/ClientDatagram.c
@@ -0,0 +1,172 @@
+/* OpDatagram_client: richiede la valutazione di un'operazione tra due interi */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define LINE_LENGTH 256
+
+//Codici errore
+#define EXIT_ARGS 1
+#define EXIT_HOST_ERR 2
+
+
+
+// client serverAddress portAddress
+
+int main(int argc, char **argv)
+{
+	struct hostent *host;
+	struct sockaddr_in clientaddr, servaddr;
+	int  port;
+	int sd;
+	int lenAddress;
+	int ris;
+
+	int lenNomeFile, lenParolaEliminare;
+	char nomefile[LINE_LENGTH];
+	char parolaEliminare[LINE_LENGTH];
+	char request[2*LINE_LENGTH];
+
+	char okstr[DIM_BUFFER];
+
+	int i = 0;
+
+	/* CONTROLLO ARGOMENTI ---------------------------------- */
+	if(argc!=3){
+		printf("Error:%s serverAddress serverPort\n", argv[0]);
+		exit(EXIT_ARGS);
+	}
+
+	//Controllo sulla porta
+	while( argv[2][i]!= '\0' ){
+		if((argv[2][i] < '0') || (argv[2][i] > '9')){
+			printf("Secondo argomento non intero\n");
+			printf("Error:%s serverAddress serverPort\n", argv[0]);
+			exit(EXIT_ARGS);
+		}
+		i++;
+	} 
+
+	//Conversione port a intero
+	port = atoi(argv[2]);
+
+	/* VERIFICA PORT e HOST */
+	if (port < 1024 || port > 65535){
+		printf("%s = porta scorretta...\n", argv[2]);
+		exit(EXIT_ARGS);
+	}
+
+	/* INIZIALIZZAZIONE INDIRIZZO CLIENT E SERVER --------------------- */
+	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr = INADDR_ANY;
+	
+	// Passando 0 ci leghiamo ad un qualsiasi indirizzo libero
+	clientaddr.sin_port = 0;
+
+	//Recupero l'indirizzo del server remoto attraverso la funzione gethostbyname.
+	host = gethostbyname (argv[1]); 
+
+	//Verifica host
+	if (host == NULL){
+		printf("%s not found in /etc/hosts\n", argv[1]);
+		exit(EXIT_HOST_ERR);
+	}else{
+		//Inizializzazione server address
+		memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr=((struct in_addr *)(host->h_addr_list[0]))->s_addr;
+		servaddr.sin_port = htons(port);
+	}
+
+	/* CREAZIONE (e controllo) SOCKET ---------------------------------- */
+
+	if((sd=socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+		perror("apertura socket");
+		exit(1);
+	}
+	printf("Client: creata la socket sd=%d\n", sd);
+
+	/* BIND SOCKET, a una porta scelta dal sistema --------------- */
+	if(bind(sd,(struct sockaddr *) &clientaddr, sizeof(clientaddr))<0) {
+		perror("bind socket ");
+		exit(1);
+	}
+	printf("Client: bind socket ok, alla porta %i\n", clientaddr.sin_port);
+
+	/* CORPO DEL CLIENT: ciclo di accettazione di richieste da utente */
+	printf("nome file, EOF per terminare: ");
+
+	while ((fgets(nomefile, LINE_LENGTH, stdin)) != NULL )
+	{
+		printf("Nome file: %s\n", nomefile);
+		printf("Inserisci la parola da eliminare:\n");
+
+        if((fgets(parolaEliminare, LINE_LENGTH, stdin)) == NULL){
+            //EOF: esco dal ciclo.
+            break;
+        }
+				
+		//lunghezza nome file:
+		//Non ho bisogno di aggiungere +1 perchè devo contare fino al \n
+		lenNomeFile = strlen(nomefile);
+
+		//lunghezza parola da eliminare:
+		//Non ho bisogno di aggiungre +1 perchè sostituisco \n con \0
+		lenParolaEliminare = strlen(parolaEliminare);
+
+		//Devo formare la richiesta:
+		//Sostituisco \n con \0
+		parolaEliminare[lenParolaEliminare-1] = '\0';
+
+		//Salvo tutto nel buffer request.
+		request[0] = '\0';
+		strcat(request, nomefile);
+		strcat(request, parolaEliminare);
+
+		//Lunghezza struttura indirizzo.
+		lenAddress=sizeof(servaddr);
+
+		if(sendto(sd, nomefile, sizeof(char)*(lenNomeFile+lenParolaEliminare), 0, (struct sockaddr *)&servaddr, lenAddress)<0){
+			perror("sendto");
+			printf("nome file, EOF per terminare: ");
+			continue;
+		}
+
+		/* ricezione del risultato */
+		printf("Attesa del risultato...\n");
+		
+		if (recvfrom(sd, &ris, sizeof(ris), 0, (struct sockaddr *)&servaddr, &lenAddress)<0){
+			perror("recvfrom");
+			printf("nome file, EOF per terminare: ");
+			continue;
+		}
+
+		//Conversione in formato locale.
+		ris = (int) ntohl(ris);
+
+		if(ris >= 0){
+			printf("Numero occorrenze eliminate : %i\n", ris);
+		}else{
+			printf("Errore dal server : %i\n", ris);
+		}
+
+		printf("nome file, EOF per terminare: \n");
+		gets(okstr); 
+		//consumo il restante della linea (\n compreso), altrimenti alla prossima iterazione la fgets avrebbe già
+        //il resto della linea da leggere
+
+	} // while gets
+	
+	//CLEAN OUT
+	close(sd);
+	printf("\nClient: termino...\n");  
+
+	return 0;
+}
---
+++ src/Server.c
@@ -0,0 +1,320 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <dirent.h>
+#include <netdb.h>
+
+#define DIM_BUFFER 256
+
+#define PARAM_ERR 1
+#define NETW_ERR 2
+#define TX_NETW_ERR 3
+#define RX_NETW_ERR 4
+#define IO_ERR 5
+#define FORK_ERR 6
+#define EXEC_ERR 7
+#define SELECT_ERR 8
+
+void gestore(int signo){
+	int stato;
+	printf("esecuzione gestore di SIGCHLD\n");
+	wait(&stato);
+}
+
+//server porta
+
+int main(int argc, char **argv) {
+    //Costante per definire REUSE_ADDR
+    const int on = 1;
+
+    //Socket descriptor per la listen e la connessione al client.
+	int listenFd, udpFd;
+    
+	int port;
+    //Lunghezza indirizzo
+    int lenAddress;
+
+    //Contatore per verifica args.
+    long num;
+	
+    //Indirizzi client e server.
+	struct sockaddr_in cliaddr, servaddr;
+    //Risoluzione host client.
+	struct hostent *host;
+
+    //Strutture per algoritmo DATAGRAM
+    int countRmWord = 0; //contatore numero di parole eliminate dal server
+    char requestDatagram[DIM_BUFFER];
+    char *nomeFile;
+    char *parola;
+
+    //Strutture per algoritmo STREAM
+    //--------------------------------------------TODO------------------------------------------------------------
+
+    //Inizializzo il buffer a zero
+    memset(requestDatagram, 0, DIM_BUFFER);
+
+	// Controllo argomenti
+	if (argc != 2) {
+		printf("Error: %s port\n", argv[0]);
+		exit(PARAM_ERR);
+	} else {
+		num = 0;
+		while (argv[1][num] != '\0') {
+			if( (argv[1][num] < '0') || (argv[1][num] > '9') ){
+				printf("Secondo argomento non intero\n");
+				exit(PARAM_ERR);
+			}
+			num++;
+		} 	
+		port = atoi(argv[1]);
+		if (port < 1024 || port > 65535) {
+			printf("Error: %s port\n", argv[0]);
+			printf("1024 <= port <= 65535\n");
+			exit(PARAM_ERR);  	
+		}
+	}
+
+	// Inizializzazione struttura server.
+	memset ((char *)&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(port);
+
+//--------------------------------------INIZIALIZZAZIONE SOCKET STREAM----------------------------------------------------
+	// Creazione socket stream!
+	listenFd = socket(AF_INET, SOCK_STREAM, 0);
+	if(listenFd < 0) {
+		perror("Errore creazione socket stream.");
+		exit(NETW_ERR);
+	}
+
+    //Imposto REUSE_ADDR per la stream
+	if(setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0){
+		perror("Errore: impossibile abilitare opzione reuse address stream socket");
+		exit(NETW_ERR);
+	}
+
+    // Eseguo il bind della socket stream
+    if (bind(listenFd,(struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)	{
+		perror("Errore binding socket stream");
+		exit(NETW_ERR);
+	}
+
+//--------------------------------------INIZIALIZZAZIONE SOCKET DATAGRAM----------------------------------------------------
+    // Creazione socket datgram
+    udpFd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(udpFd < 0){
+        perror("Errore creazione datagram socket.");
+        exit(NETW_ERR);
+    }
+
+    // Imposto REUSE_ADDR per la socket datagram
+	if(setsockopt(udpFd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0){
+		perror("Errore: impossibile abilitare opzione reuse address socket datagram");
+		exit(NETW_ERR);
+	}
+    
+    // Eseguo il bind della socket datagram
+	if (bind(listenFd,(struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)	{
+		perror("Errore binding socket stream");
+		exit(NETW_ERR);
+	}
+
+	// Creo la coda per la ricezione delle richieste di connessione socket stream
+	if (listen(listenFd, 5) < 0) {
+		perror("Errore creazione coda di ascolto."); 
+		exit(NETW_ERR);
+	}
+
+	// Imposto handler per la gestione del segnale SIG_CHLD
+	signal(SIGCHLD, gestore);
+
+//---------------------------------------TERMINATO SETTING DELLE SOCKET STREAM/UDP----------------------------------------------
+
+    // Ora pulisco le maschere e inizio a settare i fd di mio interesse che utilizzo nella select
+    fd_set fdSet;
+    FD_ZERO(&fdSet);
+
+    int maxFd = max(listenFd, udpFd) + 1;
+
+	// Ciclo infinito delle richieste che leggo tramite la select
+	for (;;) {
+        
+        // Setto le maschere per la select
+        FD_SET(listenFd, &fdSet);
+        FD_SET(udpFd, &fdSet);
+
+        if(select(maxFd, &fdSet, NULL, NULL, NULL) < 0){
+            if (errno == EINTR) //se sono stato interrotto da un segnale la ripeto
+                continue; 
+            else { //errore esco
+                perror("Errore esecuzione select");
+                close(listenFd);
+                close(udpFd);
+                exit(SELECT_ERR);    
+            }
+        }
+
+        if(FD_ISSET(listenFd, &fdSet)){
+            // Allora un cliente stream ha richiesto una connessione sulla socket d'ascolto.
+            printf("SERVER: ricevuta una richiesta di connessione...\n");
+            int len = sizeof(struct sockaddr_in);
+            int fdConnect;
+            if((fdConnect = accept(listenFd, (struct sockaddr *)&cliaddr, &len)) < 0){
+                if (errno==EINTR){
+				    perror("Forzo la continuazione della accept.");
+				    continue;
+			    } else
+                    exit(IO_ERR);
+            }
+
+            // Se tutto ok in fdConnect ho la socket per gestire la connessione
+            int pid = fork();
+
+            if(pid == 0){ // sono il figlio ciclicamente andrò a ricevere nomi di direttori (STESSA connessione)
+                close(listenFd);
+
+                int nRead;
+                char nomeFile[DIM_BUFFER];
+
+                for(;;){ // ciclo infinito di ricezione del nome del direttorio sulla stessa directory
+
+                    if ((nRead = read(fdConnect, nomeFile, sizeof(nomeFile))) < 0) {
+                        perror("Errore nella lettura del nome del file");
+                        continue;
+                    } else if (nRead == 0) {
+                        printf("SERVER STREAM (FIGLIO): ricevuto EOF, termino.\n");
+                        break;
+                    } else {
+                        // Ho letto il nome del file, logica applicativa
+                        DIR *currDir;
+                        
+                        // Verifico se esiste e ne possiedo i diritti
+                        currDir = opendir(nomeFile);
+                        if(currDir == NULL){
+                            // il file che mi hai passato o non esiste o non hai i diritti te lo comunico e rileggo
+                            char *errorMsg = "Directory non presente o non possiedi i diritti.";
+                            if (write(fdConnect, errorMsg, strlen(errorMsg)) < 0) {
+                                perror("Errore scrittura su socket connessa");
+                                closedir(currDir);
+                                close(fdConnect);
+                            }
+                            continue;
+                        }
+
+                        // Ho aperto la directory richiesta dal cliente analizzo il contenuto e invio i nomi dei file 2° liv.
+                        struct dirent *currItem;
+                        
+                        while((currItem = readdir(currDir)) != NULL){ //ciclo su tutti i file della directory
+                            if(currItem->d_type == DT_DIR){
+
+                            }
+                        } 
+                    }
+                    
+                    
+                }
+            }
+        }
+
+        if(FD_ISSET(udpFd, &fdSet)){
+            // Allora ho ricevuto un datagram sulla socket server datagram.
+        }
+
+        FD_ZERO(&fdSet); //ciclicamente risetto la maschera dei FD tutti a 0
+        
+
+
+
+        
+
+		if (fork() == 0){ // figlio itera e processa le richieste fino a quando non riceve EOF!
+			/*Chiusura FileDescr non utilizzati*/
+			close(listen_sd);
+
+            //Recupero informazioni client per stampa.
+			host = gethostbyaddr( (char *) &cliaddr.sin_addr, sizeof(cliaddr.sin_addr), AF_INET);
+			if (host == NULL){
+				perror("Impossibile risalire alle informazioni dell'host a partire dal suo indirizzo.\n");
+				continue;
+			} else 
+				printf("Server (figlio): host client è %s\n", host->h_name);
+
+            //Leggo il numero della linea da rimuovere.
+            if(read(conn_sd, &deleteLine, sizeof(long)) < 0){
+                //Non riesco a leggere la linea da eliminare esco.
+                perror("Impossibile leggere il numero della linea da eliminare.");
+                exit(IO_ERR);
+            }
+			//se tutto ok non eseguo controlli sono tutti eseguiti dal client nel caso non invia nemmeno la richiesta al server.
+			printf("Linea da cancellare: %ld\n", deleteLine);
+			
+            //Posso procedere:
+            //Inizio a leggere il file
+            //Mentre leggo conto le righe.
+            //Se la riga è quella da eliminare non invio indietro.
+            
+            //Contatore dei caratteri
+            num = 0;
+
+			//prendo tempo di start
+        	clock_t begin = clock();
+
+			while((read(conn_sd, &tmpChar, sizeof(char))) > 0){
+
+                //Salvo il carattere in un buffer temporaneo.
+                buff[num] = tmpChar;
+
+                //Se il carattere è il fine linea:
+                if(buff[num] == '\n'){
+                    //Se consideriamo come prima linea la 1 allora faccio prima:
+                    currentLine++;
+
+                    //Controllo linea da saltare
+                    if(currentLine != deleteLine){
+                        //Scrivo indietro la riga con \n finale e la stampo!
+						printf("SERVER: rispedisco indietro la linea --> %s\n", buff);
+                        write(conn_sd, buff, strlen(buff));
+                    }
+
+                    //Resetto il contatore.
+                    num = 0;
+                } else 
+					num++;
+            }
+
+			// Chiusura socket in spedizione -> invio dell'EOF
+			shutdown(conn_sd, 1);
+			shutdown(conn_sd, 0);
+       		close(conn_sd);
+
+
+			//prendo il tempo finale
+        	clock_t end = clock();
+        	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+        	printf("CLIENT: tempo di esecuzione %f sec\n", time_spent);
+
+            if(nread < 0){
+                perror("Errore lettura file");
+                exit(IO_ERR);
+            }
+
+		} // figlio
+
+        //PADRE
+		close(conn_sd);  // padre chiude socket di connessione non di ascolto
+	} // ciclo for infinito
+}
